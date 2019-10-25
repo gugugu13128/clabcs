2da51a5adc50783fa8651cea90b0a5fd1b5da6cc
gugugu13128 <1336961107@qq.com>
2019-10-25 04:38:34 -0700

    added a file

--- lab1/lb1.c
+++ lab1/lb1.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-main ()
+main()
 {
-    int fahr,celsius;
-    int 
+    float celsius,fahr;
+    int lower,upper,step;
+
+    lower=0;
+    upper=100;
+    step=5;
+    
+    celsius=upper;
+    while (fahr <= upper ){
+        celsius =(5.0/9.0)*(fahr-32.0);
+        printf("%3.0f %6.1f\n",fahr,celsius);
+        fahr = fahr + step;
+    }
